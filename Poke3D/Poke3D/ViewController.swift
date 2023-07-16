d4ce71f5199499e30f28654f5f948528fff7cfef
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-07-16 22:06:45 +0900

    Feat: Poke3D

--- Poke3D/Poke3D/ViewController.swift
+++ Poke3D/Poke3D/ViewController.swift
@@ -18,22 +18,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+//        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+
+        guard let trackingImage = ARReferenceImage.referenceImages(inGroupNamed: "Card", bundle: Bundle.main) else {
+            return
+        }
+        configuration.trackingImages = trackingImage
+        configuration.maximumNumberOfTrackedImages = 2
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -48,27 +47,60 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        guard let imageAnchor = anchor as? ARImageAnchor else {
+            return nil
+        }
+        
+        if imageAnchor.referenceImage.name == "card1" {
+            let eeveeNode = makeEeveeNode()!
+            eeveeNode[0].transform = SCNMatrix4MakeRotation(GLKMathDegreesToRadians(90), 1, 0, 0)
+            eeveeNode[0].scale = SCNVector3(0.001, 0.001, 0.001)
+            
+            node.addChildNode(eeveeNode[0])
+            node.addChildNode(eeveeNode[1])
+        }
+
+        if imageAnchor.referenceImage.name == "card2" {
+            let pikachuNode = makePikachuNode()!
+            pikachuNode[0].transform = SCNMatrix4MakeRotation(GLKMathDegreesToRadians(90), 1, 0, 0)
+            pikachuNode[0].scale = SCNVector3(0.001, 0.001, 0.001)
+            
+            node.addChildNode(pikachuNode[0])
+            node.addChildNode(pikachuNode[1])
+        }
         return node
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    // MARK: - Feature Methods
+    
+    func makePikachuNode() -> [SCNNode]? {
+        guard let pikachuScene = SCNScene(named: "art.scnassets/Pikachu/Pikachu.scn") else {
+            print("Here")
+            return nil
+        }
+        if let pikachuNode = pikachuScene.rootNode.childNode(withName: "pikachu", recursively: true) {
+            if let pikachuFNode = pikachuScene.rootNode.childNode(withName: "PikachuF", recursively: true) {
+                return [pikachuNode, pikachuFNode]
+            }
+        }
         
+        return nil
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    func makeEeveeNode() -> [SCNNode]? {
+        guard let eeveeScene = SCNScene(named: "art.scnassets/Eevee/Eevee.scn") else {
+            print("Here")
+            return nil
+        }
+        if let eeveeNode = eeveeScene.rootNode.childNode(withName: "Eevee", recursively: true) {
+            if let eeveeFNode = eeveeScene.rootNode.childNode(withName: "EeveeF", recursively: true) {
+                return [eeveeNode, eeveeFNode]
+            }
+        }
         
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        return nil
     }
 }
